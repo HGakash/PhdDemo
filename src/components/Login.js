56537ae0e8295e1d6c53a71352ddd132f02d969c
HGakash <hgakashgowda1234@gmail.com>
2025-01-02 12:41:04 +0530

    from frontend

--- src/components/Login.js
+++ src/components/Login.js
@@ -39,7 +39,7 @@ function Login(props) {
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Login to your account
+          Login to your account Now
         </h2>
         <p className="mt-2 text-center text-sm text-gray-600">
           Join your community
